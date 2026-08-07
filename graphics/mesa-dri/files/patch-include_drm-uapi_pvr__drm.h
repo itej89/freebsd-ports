4fd0a6f85a69e497a0311dbbf85f2130b91d2181
Tej Kiran <kiran.tej@amd.com>
2026-08-07 08:47:02 -0500

    mesa-dri: Also guard linux/types.h in pvr_drm.h for FreeBSD

--- graphics/mesa-dri/files/patch-include_drm-uapi_pvr__drm.h
+++ graphics/mesa-dri/files/patch-include_drm-uapi_pvr__drm.h
@@ -1,16 +1,21 @@
 --- include/drm-uapi/pvr_drm.h.orig	2025-07-10 00:00:00 UTC
 +++ include/drm-uapi/pvr_drm.h
-@@ -6,7 +6,13 @@
+@@ -6,8 +6,18 @@
 
  #include "drm.h"
 
 +#ifdef __linux__
  #include <linux/const.h>
+ #include <linux/types.h>
 +#else
++#include <stdint.h>
 +#ifndef _BITULL
 +#define _BITULL(x) (1ULL << (x))
 +#endif
++typedef uint32_t __u32;
++typedef uint64_t __u64;
++typedef int32_t __s32;
 +#endif
- #include <linux/types.h>
 
  #if defined(__cplusplus)
+ extern "C" {
