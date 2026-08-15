10be36374e77742505e54881fb4aee2cded36a12
Tej Kiran <kiran.tej@amd.com>
2026-08-14 20:56:40 -0500

    mesa-dri: Remove transfer-as-render patches, keep only FAST2D fix

--- graphics/mesa-dri/files/patch-src_imagination_vulkan_pvr__arch__job__transfer.c
+++ graphics/mesa-dri/files/patch-src_imagination_vulkan_pvr__arch__job__transfer.c
@@ -22,13 +22,3 @@
        }
        if (result != VK_SUCCESS)
           return result;
-@@ -6027,7 +6030,8 @@
- VkResult pvr_arch_transfer_job_submit(struct pvr_transfer_ctx *ctx,
-                                       struct pvr_sub_cmd_transfer *sub_cmd,
-                                       struct vk_sync *wait_sync,
--                                      struct vk_sync *signal_sync)
-+                                      struct vk_sync *signal_sync,
-+                                      struct pvr_winsys_transfer_regs *regs_out)
- {
-    list_for_each_entry_safe (struct pvr_transfer_cmd,
-                              transfer_cmd,
